247a75a8caff42315ea5c777af9abbd59ab6eee3
Dragonk1 <keyntao99@yahoo.fr>
2022-09-26 11:05:44 +0200

    background accueil +css + responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Background from './components/Body/Background';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Background />
       <Footer />
     </div>
   );
