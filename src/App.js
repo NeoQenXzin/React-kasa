7d97e315ebda57ebdddce379454698f2316050e3
Dragonk1 <keyntao99@yahoo.fr>
2022-09-26 15:06:38 +0200

    Ajout fichier datas annoncesList + gallerie-accueil + css + responsive mobile et desktop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Background from './components/Body/Background';
+import Gallery from './components/Body/Gallery';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Background />
+      <Gallery />
       <Footer />
     </div>
   );
---
+++ src/components/Body/Gallery.js
@@ -0,0 +1,20 @@
+import { annonces } from '../../datas/annoncesList.js'
+import './Gallery.css'
+
+function Gallery(){
+    return (
+        <div className="gallery-accueil">
+            {annonces.map( (annonce) => (
+               <a href="#">
+                <div className="annonce">
+                    <img src={annonce.cover} alt="images annonces"  /> 
+                    <h2>{annonce.title}</h2>
+                </div>
+               </a>
+            )
+            )}
+        </div>
+    )
+}
+
+export default Gallery
